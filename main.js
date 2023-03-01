4567436c1cadeedf1fab08ad3a08bf19ea6d0293
pberus <pedroberustein03@hotmail.com>
2023-03-01 18:32:22 -0300

    ejercicio 1 al 9

---
+++ main.js
@@ -0,0 +1,47 @@
+// EJERCICIO 1
+let miNombre = "Pedro"
+
+//EJERCICIO 2
+let miApellido = "Berustein"
+
+//EJERCICIO 3
+let miEdad = 21
+
+//EJERCICIO 4
+let miMascota = "Chispita"
+
+//EJERCICIO 5
+let edadMascota = 10
+
+//EJERCICIO 6
+console.log(miNombre)
+console.log(miApellido)
+console.log(miEdad)
+console.log(miMascota)
+console.log(edadMascota)
+
+//EJERCICIO 7
+let nombreCompleto = miNombre + " " + miApellido
+console.log(nombreCompleto)
+
+//EJERCICIO 8
+let textoPresentacion = "Mi nombre es " + miNombre + ", mi apellido es " + miApellido + " y tengo " + miEdad + " años de edad. El nombre de mi mascota era " + miMascota + " y tenia " + edadMascota + " años de edad."
+console.log(textoPresentacion)
+
+//EJERCICIO 9
+let sumaEdades = miEdad + edadMascota
+console.log(sumaEdades)
+let restaEdades = miEdad - edadMascota
+console.log(restaEdades)
+let productoEdades = miEdad * edadMascota
+console.log(productoEdades)
+let divisionEdades = miEdad / edadMascota
+console.log(divisionEdades)
+
+//EJERCICIO 10
+prompt("Cual es mi nombre?") = miNombre
+prompt("Cual es mi apellido?") = miApellido
+prompt("Cual es mi edad?") = miEdad
+prompt("Cual era el nombre de mi mascota?") = miMascota
+prompt("Cuantos años tenia mi mascota?") = edadMascota
+
