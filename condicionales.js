e77ee40639fdc9b5281a90a64ded22e9791dcd22
Pedro <pedroberustein03@hotmail.com>
2023-03-09 17:44:19 -0300

    ejercicios incompletos condicionales

---
+++ condicionales.js
@@ -0,0 +1,91 @@
+//Ejercicio 1
+/*
+let numero1 = prompt("Escriba un numero")
+let numero2 = prompt("Escriba otro numero")
+if(numero1 > numero2){
+    console.log("Es mas grande el numero1")
+}
+*/
+//Ejercicio 2
+/*
+let numero1 = prompt("Escriba un numero")
+let numero2 = prompt("Escriba otro numero")
+if(numero1 == numero2){
+    console.log("Los numeros son iguales")
+} else{
+    console.log("Los numeros son diferentes")
+}
+*/
+
+//Ejercicio 3
+/*
+let numero1 = prompt("Escriba un numero")
+let numero2 = prompt("Escriba otro numero")
+if(numero1 == numero2){
+    console.log("Los numeros son iguales")
+} else if (numero1 > numero2){
+    console.log("El numero 1 es mas grande")
+} else {
+    console.log("El numero 2 es mas grande")
+}
+*/
+
+//Ejercicio 4
+/*
+let numero1 = prompt("Escriba un numero")
+let numero2 = prompt("Escriba otro numero")
+let numero3 = prompt("Escriba un ultimo numero")
+if(numero1 < numero2){
+    if(numero1 < numero3){
+        console.log("El numero 1 es el mas chico")
+    } else {
+        console.log("El numero 3 es el mas chico")
+    } 
+} else if(numero2 < numero3){
+    console.log("El numero 2 es el mas chico")
+} else if(numero3 < numero1){
+    console.log("El numero 3 es el mas chico")
+} else {
+    console.log("Los numeros son iguales")
+}
+*/
+
+//Ejercicio 5
+/*
+let persona1 = {
+    nombre: "Pedro",
+    edad: prompt("edad1"),
+    altura: prompt("altura1"),
+}
+let persona2 = {
+    nombre: "Juan",
+    edad: prompt("edad2"),
+    altura: prompt("altura2"),
+}
+if(persona1.edad > persona2.edad){
+    console.log("persona1 es de mayor edad")
+} else {
+    console.log("persona2 es de mayor edad")
+}
+if(persona1.altura > persona2.altura){
+    console.log("persona1 es mas alta")
+} else {
+        console.log("persona2 es mas alta")
+}
+*/
+
+//Ejercicio 6
+/*
+let miNombre = prompt("nombre")
+let miEdad = prompt("edad")
+let miAltura = prompt("altura")
+let miVision = prompt("vision")
+let estaCapacitado = (miEdad >= 18) && (miAltura > 150) && (miVision >= 8)
+if (estaCapacitado){
+    console.log(`${miNombre} esta capacitado para conducir`)
+} else{
+    console.log(`${miNombre} no esta capacitado para conducir`)
+}
+*/
+
+//Seguir haciendo los ej
